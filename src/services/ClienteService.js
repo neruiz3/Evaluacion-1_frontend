78a286507d6a3be69a8c8faae13e9081acd501d9
neruiz3 <nerea.ruiz@usach.cl>
2024-10-11 22:56:24 -0300

    Proyecto cargado

---
+++ src/services/ClienteService.js
@@ -0,0 +1,23 @@
+import httpClient from "../http-common";
+
+const getAll = () => {
+    return httpClient.get('/api/v1/clientes/');
+}
+
+const create = data => {
+    return httpClient.post("/api/v1/clientes/", data);
+}
+
+const update = data => {
+    return httpClient.put('/api/v1/clientes/', data);
+}
+
+const get = id => {
+    return httpClient.get(`/api/v1/clientes/${id}`);
+}
+
+const getRut = rut => {
+    return httpClient.get(`/api/v1/clientes/${rut}`);
+}
+
+export default { getAll, create, get, update, getRut};
---
+++ src/services/CreditoService.js
@@ -0,0 +1,41 @@
+/*import httpClient from "../http-common"; // Asegúrate de que la ruta es correcta
+
+const getCreditos = () => {
+    return httpClient.get('/api/credito/');
+}
+
+const simularCredito = (data) => {
+    return httpClient.post("/api/credito/calculaSimulacion", data);
+}
+
+const revisionInicial = (data) => {
+    return httpClient.delete(`/api/credito/revisaInicial`);
+}
+const crearCredito = (data) => {
+    return httpClient.post("/api/credito/", data); 
+}
+
+const obtenerCredito = (id) => {
+    return httpClient.get(`/api/credito/${id}`);
+}
+
+const actualizarCredito = (data) => {
+    return httpClient.put('/api/credito', data); 
+}
+
+const eliminarCredito = (id) => {
+    return httpClient.delete(`/api/credito/${id}`);
+}
+
+
+
+
+export default {
+    getCreditos,
+    simularCredito,
+    crearCredito,
+    obtenerCredito,
+    actualizarCredito,
+    eliminarCredito
+};
+*/
