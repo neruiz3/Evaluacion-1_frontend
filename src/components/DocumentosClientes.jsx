175bb64923fc16cd14ea130c960aeca7c1cd4c33
neruiz3 <nerea.ruiz@usach.cl>
2024-10-16 00:54:55 -0300

    Actualizacion  y visualizacion de documentos

--- src/components/DocumentosClientes.jsx
+++ src/components/DocumentosClientes.jsx
@@ -9,8 +9,9 @@ import Paper from "@mui/material/Paper";
 import Divider from "@mui/material/Divider";
 
 
-const DocumentosClientes= () => {
+const DocumentosClientes = () => {
   const { rut } = useParams();
+  const [id, setDocumentId] = useState(null);
   const [formData, setFormData] = useState({
     comprobanteIngresos: null,
     escrituraVivienda: null,
@@ -29,39 +30,34 @@ const DocumentosClientes= () => {
     const fetchDocumentos = async () => {
       try {
         const response = await documentacionService.getByRut(rut);
+        console.log(response.data);
         const documento = response.data;
+        setDocumentId(documento.id);
         setFormData({
-          comprobanteIngresos: convertirBlob(documento.comprobanteIngresos),
-          escrituraVivienda: convertirBlob(documento.escrituraVivienda),
-          historialCrediticio: convertirBlob(documento.historialCrediticio),
-          certificadoAvaluo: convertirBlob(documento.certificadoAvaluo),
-          estadoNegocio: convertirBlob(documento.estadoNegocio),
-          planNegocio: convertirBlob(documento.planNegocio),
-          presupuestoRemodelacion: convertirBlob(documento.presupuestoRemodelacion),
-          certificadoAntiguedadLaboral: convertirBlob(documento.certificadoAntiguedadLaboral),
-          informeDeudas: convertirBlob(documento.informeDeudas),
-          fotocopiaRut: convertirBlob(documento.fotocopiaRut),
-          cuentaAhorros: convertirBlob(documento.cuentaAhorros),
+          comprobanteIngresos: documento.comprobanteIngresos ? `data:application/pdf;base64,${documento.comprobanteIngresos}` : null,
+          escrituraVivienda: documento.escrituraVivienda ? `data:application/pdf;base64,${documento.escrituraVivienda}` : null,
+          historialCrediticio: documento.historialCrediticio ? `data:application/pdf;base64,${documento.historialCrediticio}` : null,
+          certificadoAvaluo: documento.certificadoAvaluo ? `data:application/pdf;base64,${documento.certificadoAvaluo}` : null,
+          estadoNegocio: documento.estadoNegocio ? `data:application/pdf;base64,${documento.estadoNegocio}` : null,
+          planNegocio: documento.planNegocio ? `data:application/pdf;base64,${documento.planNegocio}` : null,
+          presupuestoRemodelacion: documento.presupuestoRemodelacion ? `data:application/pdf;base64,${documento.presupuestoRemodelacion}` : null,
+          certificadoAntiguedadLaboral: documento.certificadoAntiguedadLaboral ? `data:application/pdf;base64,${documento.certificadoAntiguedadLaboral}` : null,
+          informeDeudas: documento.informeDeudas ? `data:application/pdf;base64,${documento.informeDeudas}` : null,
+          fotocopiaRut: documento.fotocopiaRut ? `data:application/pdf;base64,${documento.fotocopiaRut}` : null,
+          cuentaAhorros: documento.cuentaAhorros ? `data:application/pdf;base64,${documento.cuentaAhorros}` : null,
         });
       } catch (error) {
-              console.error("Error al obtener los documentos", error);
-          }
+        console.error("Error al obtener los documentos", error);
+      }
     };
 
     fetchDocumentos();
   }, [rut]);
 
-  const convertirBlob = (data) => {
-    if (!data) return null;
-    return new Blob([new Uint8Array(data)], { type: 'application/pdf' });
-  };
-
   const handleChange = (e) => {
     const { name, files } = e.target;
     if (files && files[0]) {
       const file = files[0];
-  
-      // Verifica que sea un PDF
       if (file.type !== 'application/pdf') {
         alert('Por favor, sube un archivo PDF válido.');
         return;
@@ -74,34 +70,46 @@ const DocumentosClientes= () => {
     e.preventDefault();
     const data = new FormData();
     data.append('rut', rut);
+    // Si hay un ID de documento existente, añade el ID a la petición
+    if (id != null) {
+      data.append('id', id); // Añadir ID para actualizar el documento
+    }
     for (let key in formData) {
-      if (formData[key]) {
+      // Verifica si el documento es un archivo antes de agregarlo
+      if (formData[key] instanceof File) {
         data.append(key, formData[key], formData[key].name);
+        console.log(`Subiendo archivo: ${key}, Nombre: ${formData[key].name}`);
       }
     }
-
     try {
-      const response = await documentacionService.create(data);
+      const response = id 
+      ? await documentacionService.update(data)
+      : await documentacionService.create(data);
       console.log('Documentos enviados exitosamente', response.data);
     } catch (error) {
       console.error('Error al enviar los documentos', error);
     }
   };
 
-  const renderFileInput = (label, name, file) => (
-    <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
-      <Typography variant="h6">{label}</Typography>
-      {file && (
-        <div>
-          <Typography variant="body2">Archivo existente: {file.name || 'documento.pdf'}</Typography>
-          {/* Enlace para abrir el PDF en una nueva pestaña */}
-          <a href={URL.createObjectURL(file)} target="_blank" rel="noopener noreferrer">Ver PDF</a>
-        </div>
-      )}
-      <input type="file" name={name} onChange={handleChange} />
-    </Paper>
-  );
-
+  const renderFileInput = (label, name, file) => {
+    const fileURL = file && typeof file === 'object' ? URL.createObjectURL(file) : file;
+  
+    return (
+      <Paper elevation={3} style={{ padding: "16px", marginBottom: "16px" }}>
+        <Typography variant="h6">{label}</Typography>
+        {file ? (
+          <iframe
+            src={fileURL}
+            title={label}
+            style={{ width: '100%', height: '400px', border: 'none' }}
+          />
+        ) : (
+          <Typography color="textSecondary">No hay documento subido.</Typography>
+        )}
+        <input type="file" name={name} onChange={handleChange} style={{ marginTop: "8px" }} />
+      </Paper>
+    );
+  };
 
   return (
     <Container maxWidth="md">
@@ -111,7 +119,7 @@ const DocumentosClientes= () => {
         <br></br>
       </Typography>
       <form onSubmit={guardaDocumentacion}>
-      {renderFileInput('Comprobante de Ingresos', 'comprobanteIngresos', formData.comprobanteIngresos)}
+        {renderFileInput('Comprobante de Ingresos', 'comprobanteIngresos', formData.comprobanteIngresos)}
         {renderFileInput('Escritura Primera Vivienda', 'escrituraVivienda', formData.escrituraVivienda)}
         {renderFileInput('Historial Crediticio', 'historialCrediticio', formData.historialCrediticio)}
         {renderFileInput('Certificado de Avalúo', 'certificadoAvaluo', formData.certificadoAvaluo)}
@@ -130,4 +138,5 @@ const DocumentosClientes= () => {
     </Container>
   );
 };
+
 export default DocumentosClientes;
